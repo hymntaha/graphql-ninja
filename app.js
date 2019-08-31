f830a62a4f15365beeef875eac24973a959bce7e
taha uygun <tahauygun@gmail.com>
2019-08-30 22:48:42 -0700

    prettifying...

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const app = express();
-const {ApolloServer} = require('apollo-server-express');
+const { ApolloServer, gql } = require('apollo-server-express');
 
 const typeDefs = null;
 const resolvers = null;
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
 });
 
-server.applyMiddleware({app})
+server.applyMiddleware({ app });
 
-app.listen(9876, () => console.info('Apollo GraphQL server is running on port 3000'));
-
+app.listen(9876, () =>
+  console.info('Apollo GraphQL server is running on port 3000'),
+);
