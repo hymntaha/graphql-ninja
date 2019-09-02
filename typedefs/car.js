dcdec17b899c6d42b8cf239567a11d0c61db3f7a
taha uygun <tahauygun@gmail.com>
2019-09-01 23:21:50 -0700

    car typedefs refactor

---
+++ typedefs/car.js
@@ -0,0 +1,22 @@
+const { gql } = require('apollo-server-express');
+
+
+module.exports = gql`
+    type Query {
+        cars: [Car]
+        car(id: Int!): Car
+    }
+
+    type Mutation {
+        createCar(id: Int!, make: String!, model: String!, color: String!): Car!
+        removeCar(id: Int!): Boolean
+    }
+
+    type Car {
+        id: ID!
+        make: String!
+        model: String!
+        color: String!
+        ownder: User!
+    }
+`;
