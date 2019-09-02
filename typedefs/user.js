a6b82b0402cc8ff5b12a12c1cc8802802822a619
taha uygun <tahauygun@gmail.com>
2019-09-01 23:20:24 -0700

    refactoring typedefs for user

---
+++ typedefs/user.js
@@ -0,0 +1,22 @@
+const { gql } = require('apollo-server-express');
+
+
+module.exports = gql`
+    type Query {
+        users: [User]
+        user(id: Int!): User
+
+        me: User
+    }
+
+    type Mutation {
+        makeUser(id: Int!, name: String!): User!
+        removeUser(id: Int!): Boolean
+    }
+
+    type User {
+        id: ID!
+        name: String!
+        car: [Car]
+    }
+`;
