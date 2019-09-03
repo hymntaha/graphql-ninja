e45dfeb87978e8af0416b7deb8b14b1260f251cb
taha uygun <tahauygun@gmail.com>
2019-09-02 23:34:30 -0700

    finishing up dummy data

--- load-dummy-data.js
+++ load-dummy-data.js
@@ -1,19 +1,64 @@
 const { sequelize } = require('./models/database');
 const models = require('./models');
 
-const createData = () => {
-  models.User.create({
-    name: "Taha",
-    cars:[{
-      make: 'Mercedes',
-      model: 'C300',
-      color: 'white'
-    }]
-  },{
-    include:[models.Car]
-  })
+const createData = async () => {
+  await models.User.create(
+    {
+      name: 'Taha',
+      cars: [
+        {
+          make: 'Mercedes',
+          model: 'C300',
+          color: 'white',
+        },
+      ],
+    },
+    {
+      include: [models.Car],
+    },
+  );
+  await models.User.create(
+    {
+      name: 'Steven',
+      cars: [
+        {
+          make: 'Tesla',
+          model: 'Model X',
+          color: 'black',
+        },
+        {
+          make: 'Audi',
+          model: 'Q8',
+          color: 'red',
+        },
+      ],
+    },
+    {
+      include: [models.Car],
+    },
+  );
+  await models.User.create(
+    {
+      name: 'Susan',
+      cars: [
+        {
+          make: 'Chevy',
+          model: 'Corvette',
+          color: 'yellow',
+        },
+      ],
+    },
+    {
+      include: [models.Car],
+    },
+  );
 };
 
-sequelize.sync.then(() => {
-  createData();
+sequelize.sync.then(async () => {
+  try {
+    await createData();
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
 });
