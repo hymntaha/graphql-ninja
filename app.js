dafd5fa4142491f2da29cf8339236886634381eb
taha uygun <tahauygun@gmail.com>
2019-09-03 22:42:53 -0700

    adding cors

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 const { ApolloServer } = require('apollo-server-express');
-
+const cors = require('cors');
 const models = require('./models');
 const typeDefs = require('./typedefs');
 const resolvers = require('./resolvers');
 
 
-const me = models.users[0];
+// const me = models.users[0];
 
 
 
@@ -22,6 +22,7 @@ const server = new ApolloServer({
 });
 
 server.applyMiddleware({ app });
+app.use(cors());
 
 app.listen(9876, () =>
   console.info('Apollo GraphQL server is running on port 9876'),
