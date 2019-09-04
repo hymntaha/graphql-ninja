f27b25c3f7f3607b9b6daded9f69575d7fdfbe62
taha uygun <tahauygun@gmail.com>
2019-09-03 23:27:34 -0700

    adding mutation to add user

--- fleet-app/src/AddUser.js
+++ fleet-app/src/AddUser.js
@@ -1,54 +1,62 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import gql from 'graphql-tag';
-import {Mutation} from 'react-apollo';
+import { Mutation } from 'react-apollo';
 import query from './query';
 
-
 const addUser = gql`
-    mutation makeUser($name: String!){
-        makeUser(name: $name){
-            id,
-            name,
-            car{
-                make
-            }
-        }
+  mutation makeUser($name: String!) {
+    makeUser(name: $name) {
+      id
+      name
+      car {
+        make
+      }
     }
+  }
 `;
 
 class AddUser extends Component {
   state = {
-    name: ''
+    name: '',
   };
 
-  resetFields = () =>{
-    this.setState({name: ''})
-  }
+  resetFields = () => {
+    this.setState({ name: '' });
+  };
 
-  nameChanged = ({targer: {value}})=>{
-    this.setState({name: value});
-  }
+  nameChanged = ({ targer: { value } }) => {
+    this.setState({ name: value });
+  };
 
   render() {
     return (
-      <form onSubmit={evt => {
-        evt.preventDefault();
-      //   makeUser({
-      //     variables: {
-      //       name: this.state.name;
-      // }
-      // })
-        ;
-        this.resetFields;
-      }}>
-        <label>
-          <span>Name</span>
-          <input type="text" value={this.state.name} onChange={this.nameChanged}/>
-        </label>
-        <div>
-          <button>Add User</button>
-        </div>
-      </form>
+      <Mutation mutation={addUser} refetchQueries={[{
+        query: query
+      }]} awaitRefetchQueries={true}>
+        <form
+          onSubmit={evt => {
+            evt.preventDefault();
+            //   makeUser({
+            //     variables: {
+            //       name: this.state.name;
+            // }
+            // })
+            this.resetFields;
+          }}
+        >
+          <label>
+            <span>Name</span>
+            <input
+              type="text"
+              value={this.state.name}
+              onChange={this.nameChanged}
+            />
+          </label>
+          <div>
+            <button>Add User</button>
+          </div>
+        </form>
+      </Mutation>
     );
   }
 }
