73ca8cee02cc10fbcd81cd7ec5ae9ad97c9ec6db
taha uygun <tahauygun@gmail.com>
2019-09-03 23:15:30 -0700

    querying graphql data in react

--- fleet-app/src/User.js
+++ fleet-app/src/User.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {Query} from 'react-apollo';
+import React, { Component } from 'react';
+import { Query } from 'react-apollo';
 import query from './query';
 
 class User extends Component {
@@ -7,11 +7,35 @@ class User extends Component {
     return (
       <div>
         <Query query={query}>
-          {({ data})=>{
-            console.log(data);
-            return null;
+          {({ data }) => {
+            return (
+              <div>
+                <ul>
+                  {data.users.map(({ id, name, car }) => {
+                    const list = (
+                      <li key={id}>
+                        {' '}
+                        {name}
+                        <ul>
+                          {car.length !== 0 ? (
+                            car.map(({ make, model, color }) => (
+                              <li key={`${id}-${make}`}>
+                                {make} {model}
+                                {color}
+                              </li>
+                            ))
+                          ) : (
+                            <li>No car</li>
+                          )}
+                        </ul>
+                      </li>
+                    );
+                    return list;
+                  })}
+                </ul>
+              </div>
+            );
           }}
-
         </Query>
       </div>
     );
